1607879323b165dc9c8d528733f63dab20e40be2
KacperChabros <chabroskacper@gmail.com>
2023-02-13 11:24:15 +0100

    Add project files.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,18 @@
+﻿ /* TODO:
+  * 1. 9 fields - player1 can choose a field then player2 and and so forth
+  * 2. If chosen field is already occupied then the player should be able to choose again
+  * 3. Message if input is incorrect
+  * 4. Reset functionality
+ */
+
+
+namespace TicTacToe
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
