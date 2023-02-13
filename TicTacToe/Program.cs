a228752a90c7dd7784cbc798ee73ce0ec9b523cb
KacperChabros <chabroskacper@gmail.com>
2023-02-13 13:56:06 +0100

    Implemented checking if there is a winner, if the field is occupied, identifying the winner and selecting desired fields by players. Changes in Main method.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,12 +1,6 @@
 ﻿ /* TODO:
-  * 1. 9 fields - player1 can choose a field then player2 and and so forth
-  * 2. If chosen field is already occupied then the player should be able to choose again
-  * 3. Message if input is incorrect
-  * 4. Check if there is a winner
-  * 5. Reset functionality
+  * 1. Tie case
  */
-
-
 namespace TicTacToe
 {
     internal class Program
@@ -21,17 +15,19 @@ namespace TicTacToe
         static char playAgain = 'y';            /*Defines whether to end a program or not
                                                  *'y' -> yes, I want to play again
                                                  *'n' -> no, I want to end game */
+        static string winner = "No winner";
         static void Main(string[] args)
         {
             while(playAgain == 'y')
             {
                 StartNewGame();
-                /*while ()      //while there is no winner -> the game continues
-                { 
-                    
-                }*/
+                while (!IsWinner())      //while there is no winner -> the game continues
+                {
+                    Game();
+                    RefreshBoard();
+                }
 
-                Console.Write("\nSomeone is a winner! Would you like to play again? [y/n]: ");
+                Console.Write("\n{0} is a winner! Would you like to play again? [y/n]: ", winner);
                 char input = Console.ReadKey().KeyChar;
                 while (Char.ToLower(input) != 'y' && Char.ToLower(input) != 'n')
                 {
@@ -42,11 +38,17 @@ namespace TicTacToe
             }
         }
 
+        /// <summary>
+        /// This method prepares the environment for a new game. Sets board and turn to default
+        /// and asks which player starts the game.
+        /// </summary>
         static void StartNewGame()
         {
             for (int i = 0; i < 3; i++)        //assigning the board with default values
                 for (int j = 0; j < 3; j++)
                     board[i, j] = (char)('0' + i * 3 + j + 1);
+            turn = '1';
+            winner = "No winner";
             Console.Clear();
             Console.WriteLine("Welcome to the game we are all familiar with - Tic Tac Toe! Player1 is 'O' and player2 is 'X");
             Console.Write("Who wants to start? Please enter 1 for player1 or 2 for player2: ");
@@ -59,6 +61,10 @@ namespace TicTacToe
             }
             turn = input;
         }
+
+        /// <summary>
+        /// This method prints out the current look of the board
+        /// </summary>
         static void RefreshBoard()
         {
             Console.Clear();
@@ -71,6 +77,120 @@ namespace TicTacToe
             Console.WriteLine("     |     |     ");
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[2,0], board[2,1], board[2,2]);
             Console.WriteLine("     |     |     ");
+        }
+
+        /// <summary>
+        /// This method checks whether the field has already been selected
+        /// </summary>
+        /// <param name="row">The row of the field</param>
+        /// <param name="col">The colum of the field</param>
+        /// <returns>true if the field has already been selected and false if it has not</returns>
+        static bool IsOccupied(int row, int col)
+        {
+            if (board[row, col] == 'O' || board[row, col] == 'X')
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// This method checks whether there is a winner or not. Calls method IdentifyWinner() if there is a winner
+        /// </summary>
+        /// <returns>True if there is a winner and false if there is not</returns>
+        static bool IsWinner()
+        {
+            for(int i=0; i<3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i,1] == board[i, 2])    //horizontal check
+                {
+                    winner = IdentifyWinner(i, 0);
+                    return true;
+                }
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])   //vertical check
+                {
+                    winner = IdentifyWinner(0, i);
+                    return true;
+                }
+            }
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])   //diagonal check
+            {
+                winner = IdentifyWinner(0, 0);
+                return true;
+            }
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                winner = IdentifyWinner(0, 2);
+                return true;
+            }
+            winner = "No winner";
+            return false;
+        }
+
+        /// <summary>
+        /// This method decides whether the winner is Player1 or Player2
+        /// </summary>
+        /// <param name="row">Row of first field of the winning sequence</param>
+        /// <param name="col">Column of first field of the winning sequence</param>
+        /// <returns>"Player1" if in the first field is 'O', "Player2" if 'X' or "No winner" in other cases</returns>
+        static string IdentifyWinner(int row, int col)
+        {
+            if (board[row, col] == 'O')
+                return "Player1";
+            else if (board[row, col] == 'X')
+                return "Player2";
+            else
+                return "No winner";
+        }
+
+        /// <summary>
+        /// This method performs actual game. Allows players to select the field they want.
+        /// </summary>
+        static void Game()
+        {
+            RefreshBoard();
+            char input;
+            int inputRow, inputCol;
+            bool isOccupied;
+            switch (turn)
+            {
+                case '1':
+                    Console.Write("Player1: Choose your field! ");
+                    do
+                    {
+                        input = Console.ReadKey().KeyChar;
+                        while (input < '1' || input > '9')
+                        {
+                            Console.Write("\nPlease, enter a value between 1 and 9!: ");
+                            input = Console.ReadKey().KeyChar;
+                        }
+                        inputRow = (int)(input - '1') / 3;
+                        inputCol = (int)(input - '1') - 3 * inputRow;
+                        if(isOccupied = IsOccupied(inputRow, inputCol))
+                            Console.WriteLine("\nThis field is occupied! Select another one!");
+                    } while (isOccupied);
+                    board[inputRow, inputCol] = 'O';
+                    turn = '2';
+                    break;
+                case '2':
+                    Console.Write("Player2: Choose your field! ");
+                    do
+                    {
+                        input = Console.ReadKey().KeyChar;
+                        while (input < '1' || input > '9')
+                        {
+                            Console.Write("\nPlease, enter a value between 1 and 9!: ");
+                            input = Console.ReadKey().KeyChar;
+                        }
+                        inputRow = (int)(input - '1') / 3;
+                        inputCol = (int)(input - '1') - 3 * inputRow;
+                        if (isOccupied = IsOccupied(inputRow, inputCol))
+                            Console.WriteLine("\nThis field is occupied! Select another one!");
+                    } while (isOccupied);
+                    board[inputRow, inputCol] = 'X';
+                    turn = '1';
+                    break;
+
+            }
         }
     }
 }
